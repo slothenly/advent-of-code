2cd4d6010267e3553535249f13bdd22674eade47
Beatrice <Beatrice@DESKTOP-FOGA9V5>
2018-12-28 22:53:29 -0800

    part one successfully written, correct answer is returned.

---
+++ day1/day1/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace day1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // CHRONAL CALIBRATION
+            // One of Santa's elves have detected a calibration anomaly. An input device
+            // returns out the frequency change of the calibration tool from the last check-in.
+            // Values are returned out into a single file, I need to calculate the final value of all
+            // those inputs to detect if the device is calibrated. 
+
+            StreamReader reader = new StreamReader("calibrationOutput.txt");
+            List<string> baseInput = new List<string>();
+
+            // Read in the input from a text file and add all the lines to the baseInput as strings
+            string newLine = "";
+            while ((newLine = reader.ReadLine()) != null)
+            {
+                baseInput.Add(newLine);
+            }
+
+            int currentCalibration = CheckCalibration(baseInput);
+
+            Console.WriteLine("Current Calibration Levels: " + currentCalibration);
+            Console.ReadLine();
+        }
+
+        /// <summary>
+        /// Checks the calibration of a given input and returns the end value
+        /// </summary>
+        /// <param name="baseInput"></param>
+        private static int CheckCalibration(List<string> baseInput)
+        {
+            // Parse out each line and add it to the integer 'current'
+            int current = 0;
+            foreach (string input in baseInput)
+            {
+                char[] inputChars = input.ToCharArray();
+                char sign = inputChars[0];
+                char[] inputValueChars = new char[inputChars.Length - 1];
+
+                // Add all but the first character to a char array
+                for (int i = 1; i < inputChars.Length; i++)
+                {
+                    inputValueChars[i - 1] = inputChars[i];
+                }
+
+                // Parse the number and return it into an integero
+                int inputValue;
+                string inputValueString = new string(inputValueChars);
+                int.TryParse(inputValueString, out inputValue);
+
+                // Actually calculate the end value
+                if (inputChars[0] == '+')
+                {
+                    current += inputValue;
+                }
+                else
+                {
+                    current -= inputValue;
+                }
+            }
+
+            // Return out the resulting number
+            return current;
+        }
+    }
+}
--- day5/day5/Program.cs
+++ day5/day5/Program.cs
@@ -32,20 +32,81 @@ namespace day5
             // Pass that input into a character array
             baseChars = baseInput.ToCharArray();
             Console.WriteLine("Initial setup prepared, working...");
+            Console.WriteLine("First character in array: " + baseInput[0]);
 
             // Recursively edit the array checking for more reactions
-            Console.WriteLine("Fully reacted length: " + adjustArray(baseChars).Length);
+            Console.WriteLine("Fully reacted length: " + AdjustArray(baseChars).Length);
 
+            // Recursively edit the array checking for each letter being removed & denote shortest one
+            char shortestChainLetter = 'A';
+            int shorestChainLength = int.MaxValue;
+            for (char c = 'A'; c <= 'Z'; c++)
+            {
+                string fullyReacted = AdjustArray(RemoveAllOfType(c, baseChars));
+                Console.WriteLine("Letter: " + c.ToString() + " Length: " + fullyReacted.Length);
 
+                // Allows us to return out shortest letter and length by checking if this is the shortest one yet
+                if (fullyReacted.Length < shorestChainLength)
+                {
+                    shortestChainLetter = c;
+                    shorestChainLength = fullyReacted.Length;
+                }
+            }
+            Console.WriteLine("Shortest Chain Letter: " + shortestChainLetter.ToString());
+            Console.WriteLine("Shortest Chain Length: " + shorestChainLength.ToString());
 
             Console.ReadLine();
         }
 
         /// <summary>
+        /// Recursively adjusting array
+        /// </summary>
+        /// <param name="passedArray"></param>
+        /// <returns></returns>
+        public static string AdjustArray(char[] passedArray)
+        {
+            // FIELDS
+            bool changed = false;
+
+            // Loops through checking for reactions
+            for (int i = 0; i < passedArray.Length-1; i++)
+            {
+                // Marks off any to be removed
+                if (CheckChars(passedArray[i], passedArray[i + 1]))
+                {
+                    changed = true;
+                    passedArray[i] = '0';
+                    passedArray[i + 1] = '0';
+                }
+            }
+
+            // Remove all the zeros and create a new array to be checked or returned
+            List<char> charList = new List<char>();
+            foreach (char c in passedArray)
+            {
+                if (c != '0')
+                {
+                    charList.Add(c);
+                }
+            }
+            char[] newArray = charList.ToArray();
+
+            // Create the returning string
+            string returned = new string(newArray);
+
+            // If any changes were made, call this fxn again with the new array
+            if (changed)
+            {
+                returned = AdjustArray(newArray);
+            }
+
+            //Console.WriteLine(returned.Length);
+            return returned;
+        }
+
+        /// <summary>
         /// Checks if two characters that were passed in are opposite cases
         /// </summary>
-        /// <param name="char1"></param>
-        /// <param name="char2"></param>
         public static bool CheckChars(char char1, char char2)
         {
             // Checks if the two passed in characters are the same letter
@@ -92,30 +153,15 @@ namespace day5
         /// Removes all of a given character from the 
         /// </summary>
         /// <param name="character"></param>
-        public static void RemoveAllOfType(char character)
+        public static char[] RemoveAllOfType(char passedChar, char[] passedArray)
         {
-
-        }
-
-        /// <summary>
-        /// Recursively adjusting array
-        /// </summary>
-        /// <param name="passedArray"></param>
-        /// <returns></returns>
-        public static string adjustArray(char[] passedArray)
-        {
-            // FIELDS
-            bool changed = false;
-
-            // Loops through checking for reactions
-            for (int i = 0; i < passedArray.Length-1; i++)
+            // Loops through checking for specific character
+            for (int i = 0; i < passedArray.Length - 1; i++)
             {
-                // Marks off any to be set as null
-                if (CheckChars(passedArray[i], passedArray[i + 1]))
+                // Marks off any found to have the same character to be removed
+                if (passedArray[i].ToString().ToUpper() == passedChar.ToString().ToUpper())
                 {
-                    changed = true;
                     passedArray[i] = '0';
-                    passedArray[i + 1] = '0';
                 }
             }
 
@@ -128,19 +174,9 @@ namespace day5
                     charList.Add(c);
                 }
             }
-            char[] newArray = charList.ToArray();
 
-            // Create the returning string
-            string returned = new string(newArray);
-
-            // If any changes were made, call this fxn again with the new array
-            if (changed)
-            {
-                returned = adjustArray(newArray);
-            }
-
-            //Console.WriteLine(returned.Length);
-            return returned;
+            // Convert the list back into an array and return it
+            return charList.ToArray();
         }
     }
 }
